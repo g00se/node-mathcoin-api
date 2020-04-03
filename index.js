ab2043db86ccb1274c34f4c822d7f6534906b91c
Mikhail <mrjames@ilita.party>
2020-04-03 23:25:52 +1000

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const request = require('request-promise');
 
 
-var vk_id = 123;
-var api_token = 'apitoken';
-
-
-module.exports = class PaperGameAPI {
+module.exports = class MathCoinAPI {
    
     constructor(token, vkid) {
         
@@ -17,43 +13,43 @@ module.exports = class PaperGameAPI {
 
     }
     /*
-	* Отправить рулоны
-	* id - кому 
-	* summa - целое число, сумма перевода
-	* cb - ответ сервера
-	*/
+  * Отправить коины
+  * id - кому 
+  * summa - целое число, сумма перевода
+  * cb - ответ сервера
+  */
     send(id,summa,cb){
 
-    	request(
-    		'https://paper.12kot3k.ru/api.php',
-    		{   
-    			headers: {
-    				'Content-Type': 'application/json'
-    			},
-    			body: {
-    				method: 'send_score',
-    				token: this.api_token,
-    				to: id,
-    				amount: summa
-    			},
-    			json: true,
-    			method: 'POST'
-    		}
-    		).then((response)=>{
-    			//console.log(response);
-    			cb(response);
-    			 // {"success":true,"amount":100,"to":"{ID кому}","current":56}
+      request(
+        'https://mathbattle.12kot3k.ru/public_api.php',
+        {   
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: {
+            method: 'send_score',
+            token: this.api_token,
+            to: id,
+            amount: summa
+          },
+          json: true,
+          method: 'POST'
+        }
+        ).then((response)=>{
+          //console.log(response);
+          cb(response);
+           // {"success":true,"amount":100,"to":"{ID кому}","current":56}
 
-    		});
-    	}
-    	/*
+        });
+      }
+      /*
 * Получить баланс
 * id - чей баланс узнать
 * cb - ответ от сервера
 */
   score(id,cb){
   request(
-    'https://paper.12kot3k.ru/api.php',
+    'https://mathbattle.12kot3k.ru/public_api.php',
     {   
       headers: {
         'Content-Type': 'application/json'
@@ -77,10 +73,10 @@ module.exports = class PaperGameAPI {
  * last_tx_id - номер последнего перевода после которого показать переводы
  *
  */
- 	txList(last_tx_id,cb){
+  txList(last_tx_id,cb){
 
     request(
-            'https://paper.12kot3k.ru/api.php',
+            'https://mathbattle.12kot3k.ru/public_api.php',
             {   
                 headers: {
                     'Content-Type': 'application/json'
@@ -96,10 +92,10 @@ module.exports = class PaperGameAPI {
         ).then((response)=>{
           
           if(response['success']){
-          	var txs = response['tx_list'];
-          	cb(false,txs);
+            var txs = response['tx_list'];
+            cb(false,txs);
           }else {
-          	cb(new Error('Не удалось получить txList'));
+            cb(new Error('Не удалось получить txList'));
           }
 
           
@@ -114,14 +110,14 @@ module.exports = class PaperGameAPI {
 // "from": "422584481",
 // "to": "294109637",
 // "score": "2000",
-// "created": "2020-02-12 21:27:09"
+// "time": "2020-02-12 21:27:09"
 // },
 // {
 // "id": "38",
 // "from": "422584481",
 // "to": "294109637",
 // "score": "1000",
-// "created": "2020-02-12 21:27:09"
+// "time": "2020-02-12 21:27:09"
 // }
 // ]
 // }
@@ -132,22 +128,13 @@ module.exports = class PaperGameAPI {
 * summa - сумма
 */
 get_link(summa){
-	if(summa){
-		return "https://m.vk.com/app7361871#t"+this.vkid+"_"+summa;
-	}else {
-		return "https://m.vk.com/app7361871#t"+this.vkid;
-	}
+  if(summa){
+    return "https://m.vk.com/app6995668#t"+this.vkid+"_"+summa;
+  }else {
+    return "https://m.vk.com/app6995668#t"+this.vkid;
+  }
   
 }
 
 
 };
-
-
-
-
-
-
-
-
-
